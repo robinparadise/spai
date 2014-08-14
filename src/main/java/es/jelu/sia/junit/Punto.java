de7353b8da301ee1c3e3ff924ff94aa2d2152320
miw-upm <miw@eui.upm.es>
2014-08-14 17:52:26 +0200

    Initial

---
+++ src/main/java/es/jelu/sia/junit/Punto.java
@@ -0,0 +1,27 @@
+package es.jelu.sia.junit;
+
+public class Punto {
+    private int x, y;
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Punto() {
+        this(0, 0);
+    }
+
+    public double modulo() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public double fase() {
+        return Math.atan((double) this.y / this.x);
+    }
+
+    @Override
+    public String toString() {
+        return "Punto[" + x + "," + y + "]";
+    }
+}
