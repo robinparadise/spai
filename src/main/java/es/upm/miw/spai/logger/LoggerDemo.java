a1533a641334b6bf7fd3fa1de59d58b500186c37
miw-upm <miw@eui.upm.es>
2014-08-15 13:21:40 +0200

    logger

--- src/main/java/es/upm/miw/spai/logger/LoggerDemo.java
+++ src/main/java/es/upm/miw/spai/logger/LoggerDemo.java
@@ -7,7 +7,12 @@ public class LoggerDemo {
     static final Logger logger = LogManager.getLogger(LoggerDemo.class.getName());
 
     public static void main(String[] args) {
+        logger.fatal("Log de fatal");
         logger.error("Log de error");
+        logger.warn("Log de warning");
+        logger.info("Log de info");
+        logger.debug("Log de debug");
+        logger.trace("Log de trace");
     }
 
 }
