26d7a2fe9716d08a44d22edf17103641ebe98722
miw-upm <miw@eui.upm.es>
2014-08-25 21:38:52 +0200

    test

--- src/test/java/es/upm/miw/spai/junit/PuntoTest.java
+++ src/test/java/es/upm/miw/spai/junit/PuntoTest.java
@@ -1,39 +1,44 @@
 package es.upm.miw.spai.junit;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PuntoTest {
+    private Punto pt;
+
+    @Before
+    public void before() {
+        pt = new Punto(2, 3);
+    }
 
     @Test
     public void testPuntoIntInt() {
-        Punto pt = new Punto(2, 2);
         assertEquals(pt.getX(), 2);
-        assertEquals(pt.getY(), 2);
+        assertEquals(pt.getY(), 3);
     }
 
     @Test
     public void testPunto() {
-        Punto pt = new Punto();
+        pt = new Punto();
         assertEquals(pt.getX(), 0);
         assertEquals(pt.getY(), 0);
     }
 
     @Test
     public void testModulo() {
-        Punto pt = new Punto(2, 3);
         assertEquals(3.6055, pt.modulo(), 10e-5);
     }
 
     @Test
     public void testFase() {
-        Punto pt = new Punto(3, 3);
-        assertEquals(0.7853, pt.fase(), 10e-5);
+        Punto pt = new Punto(2, 3);
+        assertEquals(0.9828, pt.fase(), 10e-5);
     }
 
     @Test
     public void testToString() {
-        Punto pt = new Punto(2, 3);
         assertEquals("Punto[2,3]", pt.toString());
     }
 
