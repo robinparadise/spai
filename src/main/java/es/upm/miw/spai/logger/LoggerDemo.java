84067d1346a0d647480c2de879e8484a0912691e
miw-upm <miw@eui.upm.es>
2014-08-25 21:20:25 +0200

    logger

--- src/main/java/es/upm/miw/spai/logger/LoggerDemo.java
+++ src/main/java/es/upm/miw/spai/logger/LoggerDemo.java
@@ -4,9 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggerDemo {
-    static final Logger logger = LogManager.getLogger(LoggerDemo.class.getName());
-
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(LoggerDemo.class.getName());
         logger.fatal("Log de fatal");
         logger.error("Log de error");
         logger.warn("Log de warning");
